db0cfca994ceab496b21c880663229f85b1cd362
Zemian Deng <zemian.deng@oracle.com>
2020-10-01 15:38:18 -0400

    Add fibMaxSlowly async generator example

--- src/ts/fib-async-generator.ts
+++ src/ts/fib-async-generator.ts
@@ -3,14 +3,29 @@ class FibAsyncGenerator {
     b = 1;
     async *fib() {
         while(true) {
-            yield this.a;
+            yield Promise.resolve(this.a);
             // Update next value
             [this.a, this.b] = [this.b, this.a + this.b];
         }
     }
     async *fibMax(n: number) {
         while(this.a < n) {
-            yield this.a;
+            yield Promise.resolve(this.a);
+            // Update next value
+            [this.a, this.b] = [this.b, this.a + this.b];
+        }
+    }
+
+    async *fibMaxSlowly(n: number) {
+        // Yield a value every half a second.
+        while(this.a < n) {
+            let p = new Promise(resolve => {
+                setTimeout(() => {
+                    resolve(this.a);
+                }, 500);
+            });
+            yield p;
+
             // Update next value
             [this.a, this.b] = [this.b, this.a + this.b];
         }
@@ -29,4 +44,12 @@ async function testAsyncFibMax() {
         console.log(val);
     }
 }
-testAsyncFibMax();
+async function testAsyncFibMaxSlowly() {
+    let fib = new FibAsyncGenerator().fibMaxSlowly(1000);
+    for await (let val of fib) {
+        console.log(val);
+    }
+}
+// testAsyncFib();
+// testAsyncFibMax();
+testAsyncFibMaxSlowly();
