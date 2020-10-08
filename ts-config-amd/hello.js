ce5fb696ad35e36be1f12c19ebab9b8465bebd61
Zemian Deng <zemian.deng@oracle.com>
2020-10-08 12:30:15 -0400

    Add ts-config examples

---
+++ ts-config-amd/hello.js
@@ -0,0 +1,10 @@
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    exports.__esModule = true;
+    exports.hello = void 0;
+    function hello() {
+        console.log("Test");
+    }
+    exports.hello = hello;
+    hello();
+});
---
+++ ts-config-default/hello.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+exports.hello = void 0;
+function hello() {
+    console.log("Test");
+}
+exports.hello = hello;
+hello();
