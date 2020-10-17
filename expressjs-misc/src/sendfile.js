e64615e08f3e2589e68c769ec79e9b9ebd82efd6
Kenneth Deng <kennydeng352@gmail.com>
2020-10-17 16:21:00 -0400

    Remove public2

--- expressjs-misc/src/sendfile.js
+++ expressjs-misc/src/sendfile.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public2/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 app.listen(port, () => {
