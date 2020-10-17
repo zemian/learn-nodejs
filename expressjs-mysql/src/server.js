66d4e0c8f040d17af1a2784d0b363a617f793424
Kenneth Deng <kennydeng352@gmail.com>
2020-10-17 14:49:48 -0400

    Add employees listing

--- expressjs-mysql/src/server.js
+++ expressjs-mysql/src/server.js
@@ -1,10 +1,34 @@
+let mysql = require('mysql');
 let express = require('express');
 let app = express();
 
 app.use(express.static("public"));
 
-app.get("/hello", (req, res) => {
-  res.send("Hello");
+function useDB(callback) {
+  // To use MySQL8, we need to use insecureAuth=true
+  let conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'zemiannative',
+    password: 'test123',
+    database: 'employees'
+  });
+  conn.connect();
+  callback(conn);
+  conn.end();
+}
+
+app.get("/employees", (req, res) => {
+  useDB(conn => {
+    let limit = Number(req.query.limit || '10');
+    let offset = Number(req.query.offset || '0');
+    let sql = 'SELECT * FROM employees ORDER BY last_name LIMIT ?, ?';
+    console.log("Query " + sql);
+    console.log("Parameters: ", [offset, limit]);
+    conn.query(sql, [offset, limit], function (error, result) {
+      if (error) throw error;
+      res.json(result);
+    });
+  });
 });
 
 let port = 3000;
