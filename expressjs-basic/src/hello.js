269338896d0c4177992ec7b0924eb8b1a4a98700
Zemian Deng <zemiandeng@gmail.com>
2020-10-18 20:59:03 -0400

    Cleanup hello

--- expressjs-basic/src/hello.js
+++ expressjs-basic/src/hello.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
 const port = 3000;
-
-app.get('/', (req, res) => res.send('Hello World!'));
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => {
+    console.log(`Running on http://localhost:${port}`)
+});
