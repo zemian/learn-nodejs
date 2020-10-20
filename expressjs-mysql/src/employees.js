5089238d0b7e8b691f45378deb9e0f46cf6cf037
Zemian Deng <zemiandeng@gmail.com>
2020-10-20 09:02:38 -0400

    Add employees list and new actions

--- expressjs-mysql/src/employees.js
+++ expressjs-mysql/src/employees.js
@@ -5,6 +5,8 @@ let app = express();
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
+app.use(express.urlencoded({ extended: true }));
+
 function useDB(callback) {
     let conn = mysql.createConnection({
         host: 'localhost',
@@ -18,22 +20,46 @@ function useDB(callback) {
 }
 
 app.get('/', (req, res) => {
+    res.redirect('/list');
+});
+
+app.get('/list', (req, res) => {
     useDB(conn => {
-        let limit = Number(req.query.limit || '10');
         let offset = Number(req.query.offset || '0');
-        offset = offset < 1 ? 1 : offset;
+        let limit = Number(req.query.limit || '10');
         offset = offset < 0 ? 0 : offset;
-        let sql = 'SELECT * FROM employees ORDER BY last_name LIMIT ?, ?';
+        limit = limit < 0 ? 1 : limit;
+        let sql = 'SELECT * FROM employees ORDER BY hire_date desc LIMIT ?, ?';
+        let params = [offset, limit];
         console.log("Query " + sql);
-        console.log("Parameters: ", [offset, limit]);
-        conn.query(sql, [offset, limit], function (error, result) {
+        console.log("Parameters: ", params);
+        conn.query(sql, params, function (error, result) {
             if (error) throw error;
             let data = { employees: result, offset: offset, limit: limit }
             res.render('employees/list', data);
         });
     });
-
 });
+
+app.get('/new', (req, res) => {
+    res.render('employees/new');
+});
+app.post('/new', (req, res) => {
+    let form = req.body;
+    //console.log("Form submitted", form);
+    useDB(conn => {
+        let sql = `INSERT INTO employees(emp_no, first_name, last_name, gender, birth_date, hire_date) 
+        VALUES(?, ?, ?, ?, ?, ?)`;
+        let params = [Number(form.emp_no), form.first_name, form.last_name, form.gender, form.birth_date, form.hire_date];
+        console.log("Query " + sql);
+        console.log("Parameters: ", params);
+        conn.query(sql, params, function (error, result) {
+            if (error) throw error;
+            res.redirect('/list');
+        });
+    });
+});
+
 
 const port = 3000;
 app.listen(port, () => {
