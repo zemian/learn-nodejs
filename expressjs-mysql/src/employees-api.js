8c6756a196a38e7a8e37bed944123cbeb0257b7e
Zemian Deng <zemiandeng@gmail.com>
2020-10-20 18:35:01 -0400

    Add employees/new with API

--- expressjs-mysql/src/employees-api.js
+++ expressjs-mysql/src/employees-api.js
@@ -1,8 +1,10 @@
 let mysql = require('mysql');
 let express = require('express');
+const { RSA_NO_PADDING } = require('constants');
 let app = express();
 
 app.use(express.static("public"));
+app.use(express.json());
 
 function useDB(callback) {
   let conn = mysql.createConnection({
@@ -27,6 +29,26 @@ app.get("/employees/list", (req, res) => {
       if (error) throw error;
       res.json(result);
     });
+  });
+});
+
+app.post("/employees/new", (req, res) => {
+  let form = req.body;
+  //console.log("Form submitted", form);
+  useDB(conn => {
+      let sql = `INSERT INTO employees(emp_no, first_name, last_name, gender, birth_date, hire_date) 
+      VALUES(?, ?, ?, ?, ?, ?)`;
+      let params = [Number(form.emp_no), form.first_name, form.last_name, form.gender, form.birth_date, form.hire_date];
+      console.log("Query " + sql);
+      console.log("Parameters: ", params);
+      conn.query(sql, params, function (error, result) {
+          if (error) {
+            console.error("Error create new employee", error);
+            res.json({"status": "error", "message": error});
+          } else {
+            res.json({"status": "successful"});
+          }
+      });
   });
 });
 
