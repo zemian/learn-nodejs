923192c4779abde2c1271de755a2a9ecb7b05ffb
Zemian Deng <zemiandeng@gmail.com>
2021-02-18 16:45:36 -0500

    Add node-js examples

--- my-node-js/src/https-get.js
+++ my-node-js/src/https-get.js
@@ -1,25 +1,10 @@
-// // https://nodejs.org/api/https.html#https_https_get_options_callback
+// https://nodejs.org/api/https.html#https_https_get_options_callback
+//
+// https://nodejs.dev/learn/making-http-requests-with-nodejs
 
 const https = require('https');
 
-https.get('https://jsonplaceholder.typicode.com/posts', (res) => {
-    console.log('statusCode:', res.statusCode);
-    console.log('headers:', res.headers);
-
-    res.on('data', (d) => {
-        process.stdout.write(d);
-    });
-
-}).on('error', (e) => {
-    console.error(e);
-});
-
-// var options = {
-//     host: "www-proxy",
-//     port: 80,
-//     path: "https://jsonplaceholder.typicode.com/posts"
-// };
-// https.get(options, (res) => {
+// https.get('https://jsonplaceholder.typicode.com/posts', (res) => {
 //     console.log('statusCode:', res.statusCode);
 //     console.log('headers:', res.headers);
 //
@@ -30,3 +15,21 @@ https.get('https://jsonplaceholder.typicode.com/posts', (res) => {
 // }).on('error', (e) => {
 //     console.error(e);
 // });
+
+var options = {
+    host: 'www-proxy',
+    port: 80,
+    method: 'CONNECT',
+    path: "https://jsonplaceholder.typicode.com/posts",
+};
+https.get(options, (res) => {
+    console.log('statusCode:', res.statusCode);
+    console.log('headers:', res.headers);
+
+    res.on('data', (d) => {
+        process.stdout.write(d);
+    });
+
+}).on('error', (e) => {
+    console.error(e);
+});
--- my-node-js/src/https-request.js
+++ my-node-js/src/https-request.js
@@ -1,23 +1,44 @@
 // https://nodejs.dev/learn/making-http-requests-with-nodejs#perform-a-get-request
 
-const https = require('https')
-const options = {
-    hostname: 'whatever.com',
-    port: 443,
-    path: '/todos',
-    method: 'GET'
-}
+// const https = require('https')
+// const options = {
+//     hostname: 'whatever.com',
+//     port: 443,
+//     path: '/todos',
+//     method: 'GET'
+// }
+//
+// const req = https.request(options, res => {
+//     console.log(`statusCode: ${res.statusCode}`)
+//
+//     res.on('data', d => {
+//         process.stdout.write(d)
+//     })
+// })
+//
+// req.on('error', error => {
+//     console.error(error)
+// })
+//
+// req.end()
 
-const req = https.request(options, res => {
-    console.log(`statusCode: ${res.statusCode}`)
+// https://github.com/nodejs/node-v0.x-archive/issues/2474
+var http = require('http');
+var https = require('https');
 
-    res.on('data', d => {
-        process.stdout.write(d)
-    })
-})
-
-req.on('error', error => {
-    console.error(error)
-})
-
-req.end()
+var connectReq = http.request({ // establishing a tunnel
+    host: 'www-proxy',
+    port: 80,
+    method: 'CONNECT',
+    path: 'github.com:443',
+}).on('connect', function(res, socket, head) {
+    // should check res.statusCode here
+    var req = https.get({
+        host: 'github.com',
+        socket: socket, // using a tunnel
+        agent: false    // cannot use a default agent
+    }, function(res) {
+        res.setEncoding('utf8');
+        res.on('data', console.log);
+    });
+}).end();
