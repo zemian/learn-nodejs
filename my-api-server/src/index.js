2d638f59caa44930dc7b2c8235adaa9b57e6cdca
Zemian Deng <zemiandeng@gmail.com>
2021-02-19 12:19:15 -0500

    Add /endpoints discovery

--- my-api-server/src/index.js
+++ my-api-server/src/index.js
@@ -7,6 +7,12 @@ app.get("/hello", (req, res) => {
     res.send("Hello");
 });
 
+app.get("/endpoints", (req, res) => {
+    let ret = [];
+    ret.push(...app.routes.get);
+    res.send(ret);
+});
+
 let port = 3000;
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
