f80ad90e2b31cdcd5622d42ee79d72fe81d1da8c
Zemian Deng <zemiandeng@gmail.com>
2021-02-25 13:01:26 -0500

    Add endpoints.json and fix /env

--- my-api-server/src/app-env.js
+++ my-api-server/src/app-env.js
@@ -1,9 +1,16 @@
+require('dotenv').config();
 const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    console.log("Retrieving env vars");
-    res.json(process.env);
+    console.log("Retrieving APP env vars");
+    let env = {};
+    for (let key in process.env) {
+        if (key.startsWith("APP_")) {
+            env[key] = process.env[key];
+        }
+    }
+    res.json(env);
 });
 
 module.exports = router;
--- my-api-server/src/index.js
+++ my-api-server/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const endpoints = require('./endpoints.json');
 const appEnv = require('./app-env.js');
 const messages = require('./messages.js');
 
@@ -8,60 +9,12 @@ app.use('/messages', messages);
 app.use(express.static("public"));
 
 app.get("/hello", (req, res) => {
-    res.send("Hello");
+    res.json({message: "Hello", timestamp: new Date()});
 });
 
 app.get("/endpoints", (req, res) => {
-    // console.log("app._router", app._router);
-    // console.log("app.routes", app.routes);
-    let ret = [];
-
-    // Extract entpoints from main 'app'
-    if  (app.routes) {
-        for (let key in app.routes) {
-            for (let route of app.routes[key]) {
-                let path = route.path ? route.path : '/';
-                let methods = [route.method.toUpperCase()];
-                let key = path + methods.join(', ');
-                ret.push({key: key, path: path, methods: methods});
-            }
-        }
-    } else if (app._router && app._router.stack) {
-        let routes = app._router.stack.filter(e => e.route)
-        for (let routeLayer of routes) {
-            //console.log("route", routeLayer);
-            //console.log("route", routeLayer.route);
-            let route = routeLayer.route;
-            let path = route.path ? route.path : '/';
-            let methods = route.stack.map(e => e.method.toUpperCase());
-            let key = path + methods.join(', ');
-            ret.push({key: key, path: path, methods: methods});
-        }
-    }
-
-    // Extract endpoints from a router
-    ret.push(...getEndpoints('/env', messages));
-    ret.push(...getEndpoints('/messages', messages));
-
-    // Return all endpoints
-    res.send(ret);
+    res.json(endpoints);
 });
-
-function getEndpoints(prefix, router) {
-    let ret = [];
-    // console.log("prefix", router);
-    router.stack.forEach(routeLayer => {
-        // console.log("route", routeLayer.route);
-        if (routeLayer.route) {
-            let route = routeLayer.route;
-            let path = prefix + route.path;
-            let methods = route.stack.map(e => e.method.toUpperCase());
-            let key = path + methods.join(', ');
-            ret.push({key: key, path: path, methods: methods});
-        }
-    });
-    return ret;
-}
 
 let port = 3000;
 app.listen(port, () => {
